3bd1f598fdbb03b771adda0ab588be57aab63f5c
slackingslacker <slacking.slacker.coder@gmail.com>
2020-05-03 14:13:30 +0800

    added diffrent methods

--- simple.py
+++ simple.py
@@ -1,3 +1,18 @@
 import requests
 url = "https://slackingslacker.github.io/simple.html"
 print(requests.get(url).text)
+
+url = "http://slackingslacker.pythonanywhere.com"
+# Using GET Method
+print(requests.get(url+"/get").text)
+# Using POST Method
+print(requests.post(url+"/post").text)
+# Using PUT Method
+print(requests.put(url+"/put").text)
+# Using DELETE Method
+print(requests.delete(url+"/delete").text)
+# Using POST Method With FORM Submission
+print(requests.post(url+"/postdata",
+                    data={"name":"slackingslacker",
+                          "location": "earth",
+                          "height": "normal human"}).text)
