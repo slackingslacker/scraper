ed54cb55149cd974f1117e0a551179ac7a9e68a9
slackingslacker <slacking.slacker.coder@gmail.com>
2020-05-17 17:06:27 +0800

    added new selenium

---
+++ seleniumsimple.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox(executable_path="geckodriver.exe")
+driver.get("https://slackingslacker.github.io/simple.html")
+time.sleep(5)
+driver.close()
+
+driver = webdriver.Chrome(executable_path="chromedriver.exe")
+driver.get("https://slackingslacker.github.io/simple.html")
+time.sleep(5)
+driver.close()
