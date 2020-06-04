de17d225b6ea943ac39fefddbc4c277952558065
slackingslacker <slacking.slacker.coder@gmail.com>
2020-06-04 23:27:55 +0800

    added new examples

---
+++ seleniumwaittextvalue.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from datetime import datetime
+
+driver = webdriver.Firefox(executable_path="geckodriver.exe")
+driver.get("https://slackingslacker.github.io/seleniumindex#/divtags")
+
+def wait_for_text(wait_time: int, selector: str, text_value: str):
+    try:
+        print("[{}] Finding text {}".format(str(datetime.now()), selector))
+        WebDriverWait(driver, wait_time).until(
+            EC.text_to_be_present_in_element_value((By.CSS_SELECTOR, selector), text_value)
+        )
+        print("[{}] Text found".format(str(datetime.now())))
+    except TimeoutException as e:
+        print("[{}] Text did not load".format(str(datetime.now())))
+
+wait_for_text(3, "div#textExpectation > div:nth-of-type(1)", "This is an input")
+wait_for_text(6, "div#textExpectation > div:nth-of-type(2) > input", "This is an input")
+wait_for_text(9, "div#textExpectation > div:nth-of-type(1) > input", "this is an input")
+driver.close()
---
+++ seleniumwaitvisibility.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from datetime import datetime
+
+driver = webdriver.Firefox(executable_path="geckodriver.exe")
+driver.get("https://slackingslacker.github.io/seleniumindex#/divtags")
+
+def wait_for_the_elements(wait_time: int, selector: str):
+    try:
+        print("[{}] Finding element {}".format(str(datetime.now()), selector))
+        element = driver.find_element_by_css_selector(selector)
+        WebDriverWait(driver, wait_time).until(EC.visibility_of(element))
+        print("[{}] Element found".format(str(datetime.now())))
+    except TimeoutException as e:
+        print("[{}] Element did not load".format(str(datetime.now())))
+
+wait_for_the_elements(3, "div#hiddenElements > div:nth-of-type(3)")
+wait_for_the_elements(6, "div#hiddenElements > div:nth-of-type(1)")
+wait_for_the_elements(9, "div#hiddenElements > div:nth-of-type(2)")
+wait_for_the_elements(12, "div#hiddenElements > div")
+driver.close()
